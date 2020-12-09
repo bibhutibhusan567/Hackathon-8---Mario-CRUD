a74f75cff23062262564e0f25a7d9b844f94f71d
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-12-09 19:39:39 +0530

    done

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 //  Your code goes here
+const marioSchema = new mongoose.Schema({
+    name: String,
+    weight: Number
+});
 
+const marioModel = new mongoose.Model('marioChar', marioSchema);
 
 module.exports = marioModel;
